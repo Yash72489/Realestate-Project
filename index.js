b817f7afddcdc271acdc3c20d944ec6b4919c728
Yash <vishwakarmayash283@gmail.com>
2025-03-03 10:30:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// TIMELINE METHOD OF GSAP.....
+
+// NOW
+
+// Store a variable First
+let myBox = gsap.timeline();
+myBox.to("#box1",{
+    x:900,
+    duration:1,
+    delay:1,
+})
+myBox.to("#box2",{
+    x:900,
+    duration:1,
+    delay:1,
+})
+myBox.to("#box3",{
+    x:900,
+    duration:1,
+    delay:1,
+})
+myBox.to("#box4",{
+    x:900,
+    duration:1,
+    delay:1,
+})
